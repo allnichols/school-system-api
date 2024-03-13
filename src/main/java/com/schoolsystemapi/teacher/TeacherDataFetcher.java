fe01df5f765f326affce39e69a99d0d7345a4d66
Max <allenmaxnichols@gmail.com>
2024-03-13 09:45:02 -0500

    some changes

--- src/main/java/com/schoolsystemapi/teacher/TeacherDataFetcher.java
+++ src/main/java/com/schoolsystemapi/teacher/TeacherDataFetcher.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import java.util.List;
 
 @DgsComponent
-@Controller
 public class TeacherDataFetcher {
     @Autowired
     private TeacherService teacherService;
